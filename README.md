d5fcffc9827ca1761eaca625d703ae486fba7e06
Paul Dinu <38941045+paultheman@users.noreply.github.com>
2022-09-14 20:12:39 +0300

    added day4_p1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Advent_of_Code_21
+
+https://adventofcode.com/2021
---
+++ day4_p1.cpp
@@ -0,0 +1,167 @@
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+
+std::string fileName {"day4_input.txt"};
+
+std::vector<int> getBingoNumbers()
+{
+    std::vector<int> output;
+    std::ifstream inf {fileName};
+    if (!inf)
+    {
+        std::cerr << "Missing input file!\n";
+    }
+
+    while (!inf.eof())
+    {
+        std::string number;
+        std::getline(inf, number, ',');
+        output.emplace_back(stoi(number, nullptr, 10));        
+    }    
+    return output;
+}
+
+std::vector<std::vector<int>> createBoards()
+{
+    std::vector<int> board;
+    std::vector<std::vector<int>> boards;
+
+    std::ifstream inf {fileName};
+    
+    int line_counter { 0 };
+    while (!inf.eof())
+    {
+        ++line_counter;
+        std::string line;
+        std::getline(inf, line);
+      
+        if (line_counter > 1 && line != "")
+        {
+            static int bLineCounter {};
+            size_t off {};
+            do {
+                size_t sz;
+                board.push_back(std::stoi(line.substr(off), &sz));
+                off += sz;
+                ++bLineCounter;
+            } while (off < line.length());
+
+            if (bLineCounter % 25 == 0)
+            {
+                boards.push_back(board);
+                board.clear();
+            } 
+        }          
+    }
+    return boards;
+}
+
+std::vector<std::vector<bool>> createMirror (const std::vector<std::vector<int>>& boards)
+{
+    std::vector<std::vector<bool>> mirror_vect;
+    mirror_vect.resize(boards.size());
+
+    for (size_t i {0}; i < mirror_vect.size(); ++i)
+    {
+        mirror_vect[i].resize(boards[i].size());
+        std::fill(mirror_vect[i].begin(), mirror_vect[i].end(), 0);
+    }
+    return mirror_vect;
+}
+
+void findIndex(const std::vector<std::vector<int>>& boards, const int val, std::vector<std::vector<bool>>& mirror)
+{
+    for (size_t i {0}; i < boards.size(); ++i)
+    {
+        auto it { std::find (boards[i].begin(), boards[i].end(), val)};
+        if (it != boards[i].end())
+        {
+            const int index { it - boards[i].begin()};
+            // std::cout << "Board no. " << i << " Element "<< index << '\n';
+            mirror.at(i).at(index) = 1;
+        }
+    }
+}
+
+int checkRow (const std::vector<std::vector<bool>>& mirror)
+{
+    for (auto boards_it {mirror.begin()};  boards_it < mirror.end(); ++boards_it)
+    {
+        for (size_t i {0}; i < boards_it->size(); i+=5)
+        {
+            int occurances {};
+            for (auto it {boards_it->begin()+i}; it < boards_it->begin()+i+5; ++it)
+                if (*it == 1) ++occurances;
+            if (occurances == 5)
+            {
+                std::cout << "Winning row in board no " << boards_it - mirror.begin() << " row " << i/5 << '\n';
+                return boards_it - mirror.begin();
+            }
+        }
+    }
+    return -1; 
+}
+
+int checkColumn (const std::vector<std::vector<bool>>& mirror)
+{
+    for (auto boards_it {mirror.begin()}; boards_it < mirror.end(); ++boards_it)
+    {
+        for (size_t i {}; i < 5; ++i)
+        {
+            int occurances {};
+            for (auto it {boards_it->begin()+i}; it < boards_it->end(); it+=5)
+                if (*it == 1) ++occurances;
+            if (occurances == 5)
+            {
+                std::cout << "Winning board no " << boards_it-mirror.begin() << " column " << i << '\n';
+                return boards_it-mirror.begin();
+            }
+        }
+    }
+    return -1;
+}
+
+int64_t calculateBoard (const std::vector<std::vector<int>>& boards, const std::vector<std::vector<bool>>& mirror, const int number, const int index)
+{
+    int64_t sum {};
+
+    for (size_t i {0}; i < mirror[index].size(); ++i)
+    {
+        if (mirror[index].at(i) == 0)
+        {
+            sum += boards[index].at(i);
+        }
+    }
+    std::cout << "Score " << sum * number << '\n';
+    return sum * number;
+}
+
+const std::vector<int> input_numbers { getBingoNumbers()};
+const std::vector<std::vector<int>> boards { createBoards() };
+std::vector<std::vector<bool>> mirror {createMirror(boards)};
+
+int main()
+{     
+    for (const auto& each : input_numbers)
+    {
+        findIndex(boards, each, mirror);
+        int row {checkRow(mirror)};
+        int column {checkColumn(mirror)};
+
+        if (row != -1)
+        {
+            std::cout << "Winning number " << each << '\n';
+            calculateBoard(boards, mirror, each, row);
+            break;
+        }
+        if (column != -1)
+        {
+            std::cout << "Winning number " << each << '\n';
+            calculateBoard(boards, mirror, each, column);
+            break;
+        }
+    }
+    return 0;
+}
